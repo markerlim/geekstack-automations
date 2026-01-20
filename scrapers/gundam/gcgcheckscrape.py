bc87697cb1265778092e5f5f2b9e4d71c2b914d1
Marcus Lim <markerlim15@gmail.com>
2026-01-20 10:29:08 +0800

    added extra to watchlist

--- scrapers/gundam/gcgcheckscrape.py
+++ scrapers/gundam/gcgcheckscrape.py
@@ -11,12 +11,14 @@ from gundamscrape import scrape_gundam_cards
 
 # Variables
 FILE_PATH = "gundamtcgdb/series.json"
+WATCHLIST_PATH = "gundamtcgdb/watchlist.json"
 BASE_URL = "https://www.gundam-gcg.com"
 C_GUNDAM = os.getenv('C_GUNDAM')
 
 # Initialize GitHub service
 mongo_service = MongoService()
 api_service = ApiService(BASE_URL)
+github_service = GitHubService()
 
 def check_for_new_series():
     """Check for new Gundam series by comparing scraped data with mongoDB distinct values"""
@@ -29,7 +31,7 @@ def check_for_new_series():
     scraped_values = [link['data-val'] for link in package_links if link['data-val']]  # Get non-empty data-val values
 
     # Step 2: Get the existing series.json file from GitHub using GitHubService
-    #existing_values, file_sha = github_service.load_json_file(FILE_PATH)
+    #existing_values, file_sha = github_service.load_json_file(FILE_PATH) series.json to deprecate
     existing_values = mongo_service.get_unique_values(C_GUNDAM,"package")
     if existing_values is None:
         print(f"Error fetching data from MongoDB collection: {C_GUNDAM}")
@@ -50,7 +52,8 @@ def check_for_new_series():
             scrape_gundam_cards(val)
 
 def check_for_watchlist():
-    watchlist = ["619701","619801","619901","619103"]
+
+    watchlist = ["619701","619801","619901","619103","619008","619007"]
 
     for package_value in watchlist:
         print(f"Checking package: {package_value}")
